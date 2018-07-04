f78b638d0e9b50064fb4e42df1ed5286346bd8ab
Anders Kravis <anders@finimize.com>
2018-07-04 16:40:04 -0400

    frame styling

--- my-app/src/components/TopNav.js
+++ my-app/src/components/TopNav.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import './TopNav.css';
 
 class TopNav extends Component {
   render() {
     return (
-      <div>
-          <p>Top Nav</p>
+      <div className="topNav">
+          <div className="topNav-left">
+						<h1> Meeting Name </h1>
+						<div className="button"> Set time </div>
+						<div className="button"> Add invites </div>
+					</div>
       </div>
     );
   }
--- my-app/src/containers/builder/Builder.js
+++ my-app/src/containers/builder/Builder.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
 import TopNav from '../../components/TopNav.js';
+import './Builder.css';
 
 class Builder extends Component {
+
+
   render() {
     return (
 			<div>
 			<TopNav/>
-      <div>
-          <p>Hello World</p>
+      <div className="container">
+
+					<h2>Meeting schedule</h2>
+
+					<div className="calendar">
+
+
+						<div className="times">
+							<span>2pm</span>
+							<span>3pm</span>
+						</div>
+
+						<div className="block-holder">
+
+							<div className="block-row">
+								<div className="block">
+									<h3>Block Name</h3>
+									<span className="block-time">30 mins</span>
+									<p className="block-description">This is the meeting description</p>
+								</div>
+							</div>
+
+							<div className="block-row">
+								<div className="block">
+									<h3>Block Name</h3>
+									<span className="block-time">30 mins</span>
+									<p className="block-description">This is the meeting description</p>
+								</div>
+							</div>
+
+							<div className="block-row">
+								<div className="block-placeholder">
+									<p>Add a block</p>
+								</div>
+							</div>
+
+							<div className="block-row">
+							</div>
+
+						</div>
+
+					</div>
+
       </div>
 			</div>
     );
