ec9256919a928393f21cc751555fdd2f8e472037
Anders Kravis <anders@finimize.com>
2018-07-06 08:58:23 -0400

    updating numbers

--- my-app/src/containers/App.js
+++ my-app/src/containers/App.js
@@ -1,28 +1,12 @@
 import React, { PureComponent } from 'react';
 import Builder from './builder/Builder.js';
 
-function App() {
-
-	var BLOCKS = [
-		{
-			title: "👋 Test",
-			span: 15,
-			description: "notes",
-		},
-		{
-			title: "Test 2",
-			span: 15,
-			description: "notes 2",
-		},
-		{
-			title: "Test 3",
-			span: 15,
-			description: "notes 3",
-		}
-	];
+class App extends PureComponent {
+	render() {
 		return (
-			<Builder blocks={BLOCKS}/>
+			<Builder />
 		)
+	}
 }
 
 export default App
--- my-app/src/containers/builder/Builder.js
+++ my-app/src/containers/builder/Builder.js
@@ -4,65 +4,33 @@ import TopNav from '../../components/TopNav.js';
 import './Builder.css';
 
 
-
-function Block(props) {
-	return (
-	<div className="block-row">
-		<div className="block">
-			<h3>{props.title}</h3>
-			<span className="block-time">{props.span ? props.span + " mins" : ""}</span>
-			<p className="block-description">{props.description}</p>
-		</div>
-	</div>
-);
-}
-
-Block.propTypes = {
-  title: PropTypes.string.isRequired,
-	span: PropTypes.number.isRequired,
-	description: PropTypes.string
-};
-
-Block.defaultProps = {
-  title: "Block Name",
-	span: 0,
-	description: "Add meeting notes"
-};
-
-
 function Application(props) {
 
+	var BLOCKS = [
+		{
+			title: "👋 Test",
+			description: "notes",
+			id: 1,
+		},
+		{
+			description: "notes 2",
+			id: 2,
+		},
+		{
+			title: "Test 3",
+			description: "notes 3",
+			id: 3,
+		}
+	];
 
     return (
 			<div>
 			<TopNav/>
       <div className="container">
 
-					<h2>Title</h2>
+					<h2>Meeting Schedule</h2>
 
-					<div className="calendar">
-
-
-						<div className="times">
-							<span>2pm</span>
-							<span>3pm</span>
-						</div>
-
-						<div className="block-holder">
-
-						{props.blocks.map(function(block) {
-							return <Block title={block.title} span={block.span} description={block.description}/>
-						})}
-
-							<div className="block-row">
-								<div className="block-placeholder">
-									<p>Add a block</p>
-								</div>
-							</div>
-
-						</div>
-
-					</div>
+					<Calendar blocks={BLOCKS} />
 
 					<div className="go-section">
 							<p>Ready to go? Send invites</p>
@@ -74,9 +42,84 @@ function Application(props) {
 }
 
 Application.propTypes = {
-	players: PropTypes.array.isRequired,
+	blocks: PropTypes.array,
 };
 
+function Calendar(props) {
+
+	return (
+	<div className="calendar">
+
+
+		<div className="times">
+			<span>2pm</span>
+			<span>3pm</span>
+		</div>
+
+		<div className="block-holder">
+
+		{props.blocks.map(function(block) {
+			return (
+			<Block title={block.title}
+			span={block.span}
+			description={block.description}
+			id={block.id}
+			key={block.id}/>
+			)
+		})}
+
+			<div className="block-row">
+				<div className="block-placeholder">
+					<p>Add a block</p>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+);
+}
+
+class Block extends React.Component {
+
+	constructor(props) {
+	super(props);
+	this.state = {
+		span: 1,
+	}
+	this.changeSpan = this.changeSpan.bind(this);
+}
+
+changeSpan() {
+	console.log('hi')
+	this.setState({
+        span : this.state.span + 1
+    });
+}
+
+	render() {
+		return (
+			<div className="block-row">
+				<div className="block" onClick={this.changeSpan}>
+					<h3>{this.props.title}</h3>
+					<span className="block-time">{this.state.span ? this.state.span + " mins" : ""}</span>
+					<p className="block-description">{this.props.description}</p>
+				</div>
+			</div>
+		)
+	}
+}
+
+Block.propTypes = {
+  title: PropTypes.string.isRequired,
+	description: PropTypes.string,
+	id: PropTypes.number.isRequired,
+};
+
+Block.defaultProps = {
+  title: "Block Name",
+	description: "Add meeting notes"
+};
 
 
 export default Application;
